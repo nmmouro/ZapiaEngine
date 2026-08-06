10a10280567aa3a1ce4dbeb2d9982a6dd4289fa9
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-06 20:14:20 -0300

    Update veiculos.js

--- js/pages/veiculos.js
+++ js/pages/veiculos.js
@@ -1,102 +1,20 @@
 // ============================================================================
-// VEÍCULOS
+// VEÍCULOS ENGINE
 // Painel Frota
 // Arquivo: js/pages/veiculos.js
 // Responsável pela inicialização da página.
 // ============================================================================
 
-import {
-
-    iniciarRelogio
-
-} from "../utils/relogio.js";
-
-import {
-
-    formulario,
-    btnNovo
-
-} from "../controllers/veiculos.state.js";
-
-import {
-
-    registrarEventos
-
-} from "../controllers/veiculos.events.js";
-
-import {
-
-    carregarTabela
-
-} from "../controllers/veiculos.helpers.js";
-
-import {
-
-    mostrarLoading,
-    esconderLoading
-
-} from "../ui/loading.js";
-
-import {
-
-    tratarErro
-
-} from "../utils/errors.js";
-
-import {
-    
-    iniciarFullscreen
-
-} from "../utils/fullscreen.js";
-
-// ============================================================================
-// INICIALIZAÇÃO
-// ============================================================================
+import crud from "../crud/crud.veiculos.js";
 
 document.addEventListener(
 
     "DOMContentLoaded",
 
-    init
+    async () => {
+
+        await crud.load();
+
+    }
 
 );
-
-// ============================================================================
-// INIT
-// ============================================================================
-
-async function init() {
-
-    try {
-
-        mostrarLoading();
-
-        iniciarFullscreen();
-
-        iniciarRelogio();
-
-        registrarEventos(
-
-            formulario,
-
-            btnNovo
-
-        );
-
-        await carregarTabela();
-
-    }
-
-    catch (erro) {
-
-        tratarErro(erro);
-
-    }
-
-    finally {
-
-        esconderLoading();
-
-    }
-
-}
